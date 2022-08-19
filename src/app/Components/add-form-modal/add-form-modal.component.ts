d21abbb546403352242eb6694bd38aff691f5f68
aldaine-AG <aldaineclarke1@gmail.com>
2022-08-19 00:47:15 -0500

    created the component for add form using modal

---
+++ src/app/Components/add-form-modal/add-form-modal.component.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/member-ordering */
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-add-form-modal',
+  templateUrl: './add-form-modal.component.html',
+  styleUrls: ['./add-form-modal.component.scss'],
+})
+export class AddFormModalComponent implements OnInit {
+  name = 'string';
+  constructor(private modalCtrl: ModalController) { }
+  addItemForm = new FormGroup({
+    item_name: new FormControl(''),
+    price: new FormControl(''),
+    quantity:new FormControl(''),
+  });
+  ngOnInit() {}
+  cancel() {
+    return this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    return this.modalCtrl.dismiss(this.name, 'confirm');
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Component } from '@angular/core';
-
+import { Item } from '../Interfaces/item.interface';
+import { ModalController } from '@ionic/angular';
+import { AddFormModalComponent } from '../Components/add-form-modal/add-form-modal.component';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +11,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
+  items: string[] = ["Hey"];
 
+  async openModal(){
+    console.log("Clicked");
+    const modal = await this.modalCtrl.create({component:AddFormModalComponent});
+    modal.present();
+
+    const {data, role} = await modal.onWillDismiss();
+    if(role === 'confirm'){
+
+    }
+  }
 }
