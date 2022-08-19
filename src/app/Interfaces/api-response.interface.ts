9ea51ae4c3602298543a8f47799ab4f8e044a920
aldaine-AG <aldaineclarke1@gmail.com>
2022-08-18 23:13:20 -0500

    updated response interface to have optional data property and have a function to handle errors

--- src/app/Interfaces/api-response.interface.ts
+++ src/app/Interfaces/api-response.interface.ts
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/quotes */
+import { of } from "rxjs";
+
 export interface Response<T>{
     status: string;
     message: string;
-    data: T;
+    data?: T;
 }
+
+export const handleHttpErrors = (data) => {console.log(data); return of(data);};
